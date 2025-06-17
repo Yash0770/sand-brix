a02d421a1a6cc39f130275b66c2182ec856ce4e8
Yash0770 <yashsolanki0728@gmail.com>
2025-06-17 13:21:48 +0530

    changes related to import and bg

--- src/app/component/Home.tsx
+++ src/app/component/Home.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import Banner from "./Banner";
+import LiveHighlightSection from "./section/LiveHighlistSection";
+import NewsSection from "./section/NewsSection";
+import TrendingSection from "./section/TrendingSection";
+import ViewAllSection from "./section/ViewAllSection";
+import VideoPlaySection from "./section/VideoPlaySection";
+import PopularLiveStreamingSection from "./section/PopularLiveStreamingSection";
+import RecentlyViewedSection from "./section/RecentlyViewedSection";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Banner />
+      <LiveHighlightSection />
+      <NewsSection />
+      <TrendingSection />
+      <ViewAllSection />
+      <VideoPlaySection />
+      <PopularLiveStreamingSection />
+      <RecentlyViewedSection />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
-'use client'
-import Header from "./component/Header";
-import Banner from "./component/Banner";
-import LiveHighlightSection from "./component/section/LiveHighlistSection";
-import NewsSection from "./component/section/NewsSection";
-import ViewAllSection from "./component/section/ViewAllSection";
+"use client";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import VideoPlaySection from "./component/section/VideoPlaySection";
-import PopularLiveStreamingSection from "./component/section/PopularLiveStreamingSection";
-import RecentlyViewedSection from "./component/section/RecentlyViewedSection";
-import Footer from "./component/Footer";
-import TrendingSection from "./component/section/TrendingSection";
+import Home from "./component/Home";
 
-export default function Home() {
+export default function page() {
   return (
-    <>
-      <Header />
-      <Banner />
-      <LiveHighlightSection />
-      <NewsSection />
-      <TrendingSection/>
-      <ViewAllSection />
-      <VideoPlaySection/>
-      <PopularLiveStreamingSection />
-      <RecentlyViewedSection />
-      <Footer/>
-    </>
+    <div className="bg-[#170000]">
+      <Home />
+    </div>
   );
 }
