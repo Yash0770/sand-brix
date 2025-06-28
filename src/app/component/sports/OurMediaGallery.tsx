17846d767d4d0588a13d53c25a6b6858dfebf40f
nitin@styxsports.com <nitin@styxsports.com>
2025-06-28 12:13:58 +0530

    sports lading page final ui update

--- src/app/component/sports/OurMediaGallery.tsx
+++ src/app/component/sports/OurMediaGallery.tsx
@@ -75,11 +75,14 @@ export default function OurMediaGallery(){
   ))}
 </div>
 
-<div className="mt-3">
-  <button className="w-full text-white text-sm font-semibold py-2 bg-[#282828] rounded-sm">
-    Load More +
-  </button>
-</div>
+   <div className="mt-6 w-full cursor-pointer">
+          <button className="w-full text-center text-white text-sm font-semibold py-3 group bg-[#282828] rounded-sm cursor-pointer">
+            <span className="inline-block relative text-[#9F9F9F]">
+              Load More +
+              <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#9F9F9F] transition-all duration-300 group-hover:w-full"></span>
+            </span>
+          </button>
+        </div>
 
      
       </div>
